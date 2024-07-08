b7dedd502710d412f9cbbd07892e1b134c2f7349
retini <retini@open2b.com>
2024-07-08 15:52:01 +0200

    Update the sample application
    This commit updates the sample application by adding buttons to test the
    `identify` call more thoroughly.

--- samples/kotlin-basic-app/src/main/java/com/segment/analytics/next/MainActivity.kt
+++ samples/kotlin-basic-app/src/main/java/com/segment/analytics/next/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.segment.analytics.kotlin.core.Analytics
+import com.segment.analytics.kotlin.core.utilities.toJsonElement
+import kotlinx.serialization.json.buildJsonObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,9 +34,19 @@ class MainActivity : AppCompatActivity() {
 
         val identifyButton: Button = findViewById(R.id.identifyButton)
         identifyButton.setOnClickListener {
-            analytics.identify("user1234")
+            analytics.identify("user1234", buildJsonObject { put("Name", "Jhon".toJsonElement()) })
         }
-        
+
+        val identifyWithEmailButton: Button = findViewById(R.id.identifyWithEmailButton)
+        identifyWithEmailButton.setOnClickListener {
+            analytics.identify("", buildJsonObject { put("Email", "user1234@example.com".toJsonElement()) })
+        }
+
+        val identifyWithAgeButton: Button = findViewById(R.id.identifyWithAgeButton)
+        identifyWithAgeButton.setOnClickListener {
+            analytics.identify("", buildJsonObject { put("Age", "35".toJsonElement()) })
+        }
+
         val resetButton: Button = findViewById(R.id.resetButton)
         resetButton.setOnClickListener {
             analytics.reset()
