2f4528ef69e5c19d8ca812f5327af1c874b2973f
retini <retini@open2b.com>
2024-07-08 15:47:55 +0200

    Standardize line endings and indentation

--- core/src/main/java/com/segment/analytics/kotlin/core/Session.kt
+++ core/src/main/java/com/segment/analytics/kotlin/core/Session.kt
@@ -1,12 +1,12 @@
 package com.segment.analytics.kotlin.core
 
 fun newSession(id: Long?, timeout: Long): SessionInfo {
-        var sessionId = id
-        val now = now()
-        if (sessionId == null) {
-            sessionId = now()
-        }
-        val expiration = now + timeout
-        return SessionInfo(sessionId, expiration, true)
+    var sessionId = id
+    val now = now()
+    if (sessionId == null) {
+        sessionId = now()
+    }
+    val expiration = now + timeout
+    return SessionInfo(sessionId, expiration, true)
 }
 
