f385ecb2b5adb03a646a2812d2746b6beb742956
retini <retini@open2b.com>
2024-08-21 14:29:00 +0200

    Update default endpoint to check if it fixes the tests

--- core/src/main/java/com/segment/analytics/kotlin/core/Constants.kt
+++ core/src/main/java/com/segment/analytics/kotlin/core/Constants.kt
@@ -2,5 +2,5 @@ package com.segment.analytics.kotlin.core
 
 object Constants {
     const val LIBRARY_VERSION = "1.16.4"
-    const val DEFAULT_ENDPOINT = "example.com/api/v1"
+    const val DEFAULT_ENDPOINT = "cdn-settings.example.com/v1"
 }
